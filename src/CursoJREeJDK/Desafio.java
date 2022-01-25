f464a83075aad72e19f145596925ed161f287308
Giovana Victória <giovanavictoria887@gmail.com>
2022-01-25 19:20:46 -0300

    In progress

---
+++ src/CursoJREeJDK/Desafio.java
@@ -0,0 +1,11 @@
+package CursoJREeJDK;
+
+public class Desafio {
+    public static void main(String[] args) {
+        for(int multiplo = 1; multiplo < 100; multiplo++) {
+           if(multiplo % 3 == 0) {
+               System.out.println(multiplo);
+           }
+        }
+    }
+}
