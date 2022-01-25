b5ef36721741d9060702f6501bffe3472fff3ac1
Giovana Victória <giovanavictoria887@gmail.com>
2022-01-25 18:52:32 -0300

    In progress

--- src/CursoJREeJDK/TestaWhile.java
+++ src/CursoJREeJDK/TestaWhile.java
@@ -1,2 +1,13 @@
-package CursoJREeJDK;public class TestaWhile {
+package CursoJREeJDK;
+
+public class TestaWhile {
+    public static void main(String[] args) {
+        int contador = 0;
+        while (contador <= 10) {
+            System.out.println(contador);
+            // contador = contador + 1; mesma coisa da linha 9
+            // contador += 1; mesma coisa da linha 10
+            contador++;
+        }
+    }
 }
