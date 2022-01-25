7bd1e7cf19a4337346adadef1f411456fb1a4447
Giovana Victória <giovanavictoria887@gmail.com>
2022-01-25 14:18:37 -0300

    In progress

---
+++ src/CursoJREeJDK/TestaCondicional2.java
@@ -0,0 +1,19 @@
+package CursoJREeJDK;
+
+public class TestaCondicional2 {
+    public static void main(String[] args) {
+        System.out.println("Testando condicionais");
+
+        int idade = 19;
+        int quantidadePessoas = 3;
+        boolean acompanhado = quantidadePessoas >= 2;
+
+        System.out.println("Está acompanhado? " + acompanhado);
+
+        if (idade >= 18 && acompanhado) { // && = E - || = OU
+            System.out.println("Seja bem vindo");
+        } else {
+            System.out.println("Menor de 18 anos, não pode entrar");
+        }
+    }
+}
