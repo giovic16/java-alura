957ef25e4f5c0d1e89d5c0ed701bb6fcac6bf89e
Giovana Victória <giovanavictoria887@gmail.com>
2022-01-24 21:49:42 -0300

    In progress

---
+++ src/CursoJREeJDK/TestaValores.java
@@ -0,0 +1,11 @@
+package CursoJREeJDK;
+
+public class TestaValores {
+    public static void main(String[] main) {
+        int primeiro = 5;
+        int segundo = 7;
+        segundo = primeiro;
+        primeiro = 10;
+        System.out.println(segundo);
+    }
+}
